a6baee1b8850d760a2aa7984e198f56167dd8b8c
Troy Dorman <troydorman@gmail.com>
2021-01-05 14:14:13 -0700

    got routes integrated into the main server file

--- server.js
+++ server.js
@@ -1,23 +1,45 @@
+/**
+ * BASIC SERVER CONFIGURATION 
+ */
+
 const express = require('express');
 const expressHandlebars = require('express-handlebars');
-
 const app = express();
-
 const port = process.env.PORT || 3000
+
+
+/**
+ * MIDDLEWEAR
+ */
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(__dirname + '/public'));
+
+
+/**
+ * HANDLEBARS INTEGRATION
+ */
 
 app.engine('handlebars', expressHandlebars({
 
     defaultLayout: 'main'
 
 }));
-
 app.set('view engine', 'handlebars');
 
 
+/**
+ * ROUTE INTEGRATION
+ */
+
+const routes = require('./controllers/burgers_controller');
+app.use(routes);
+
+
+/**
+ * LAUNCH THE SERVER
+ */
 
 app.listen(port, () => console.log(
 
@@ -25,7 +47,3 @@ app.listen(port, () => console.log(
     `press Crtl-C to terminate.`
 
 ));
-
-
-
-
