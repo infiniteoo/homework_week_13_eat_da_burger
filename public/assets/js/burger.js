6f95515fbf4d023e6600336f1da9f046207ba642
Troy Dorman <troydorman@gmail.com>
2021-01-10 14:17:39 -0700

    finalized refactoring & added proper credit to front end burger.js

--- public/assets/js/burger.js
+++ public/assets/js/burger.js
@@ -1,40 +1,42 @@
+/**
+ *  server side javascript to handle events and html methods
+ *  props to meech the dj for inspiration on this code
+ */
+
 $(function () {
-    $(".card-body").on("submit", (event) => {
+    $(`.card-body`).on(`submit`, (event) => {
 
         event.preventDefault();
 
-        let newBurger = {
-            burger_name: $("#burger_name").val().trim(),
+        let burgerToAdd = {
+            burger_name: $(`#burger_name`).val().trim(),
             devoured: 0,
         };
 
-     
-        $.ajax("/api/burgers", {
-            type: "POST",
-            data: newBurger,
+        $.ajax(`/api/burgers`, {
+            type: `POST`,
+            data: burgerToAdd,
         }).then(() => location.reload());
     });
 
-    $(".eat").on("click", function () {
-        let id = $(this).data("id");
-        let newDevoured = $(this).data("newDevoured");
+    $(`.eat`).on(`click`, function () {
+        let id = $(this).data(`id`);
+        let newlyDevoured = $(this).data(`newDevoured`);
 
         let newDevouredBurger = {
-            devoured: newDevoured,
+            devoured: newlyDevoured,
         };
 
         $.ajax(`/api/burgers/${id}`, {
-            type: "PUT",
+            type: `PUT`,
             data: newDevouredBurger,
         }).then(() => location.reload());
     });
 
-    $(".trash").on("click", function () {
-        let id = $(this).data("id");
+    $(`.trash`).on(`click`, function () {
+        let id = $(this).data(`id`);
         $.ajax(`/api/burgers/${id}`, {
-            type: "DELETE",
+            type: `DELETE`,
         }).then(() => location.reload());
     });
-
-
 });
