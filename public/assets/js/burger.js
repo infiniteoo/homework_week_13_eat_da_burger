210abb6e7b0a08ecacdba9b27c54dccf2ad5ffa1
Troy Dorman <troydorman@gmail.com>
2021-01-10 14:10:09 -0700

    more refactoring.. this time it didnt break!

--- public/assets/js/burger.js
+++ public/assets/js/burger.js
@@ -1,37 +1,40 @@
-$(() => {
-    $(`.card-body`).on(`submit`, (event) => {
+$(function () {
+    $(".card-body").on("submit", (event) => {
 
         event.preventDefault();
 
         let newBurger = {
-            burger_name: $(`#burger_name`).val().trim(),
+            burger_name: $("#burger_name").val().trim(),
             devoured: 0,
         };
 
-        $.ajax(`/api/burgers`, {
-            type: `POST`,
+     
+        $.ajax("/api/burgers", {
+            type: "POST",
             data: newBurger,
         }).then(() => location.reload());
     });
 
-    $(`.eat`).on(`click`, () => {
-        let id = $(this).data(`id`);
-        let newDevoured = $(this).data(`newDevoured`);
+    $(".eat").on("click", function () {
+        let id = $(this).data("id");
+        let newDevoured = $(this).data("newDevoured");
 
         let newDevouredBurger = {
             devoured: newDevoured,
         };
 
         $.ajax(`/api/burgers/${id}`, {
-            type: `PUT`,
+            type: "PUT",
             data: newDevouredBurger,
         }).then(() => location.reload());
     });
 
-    $(`.trash`).on(`click`, () => {
-        let id = $(this).data(`id`);
+    $(".trash").on("click", function () {
+        let id = $(this).data("id");
         $.ajax(`/api/burgers/${id}`, {
-            type: `DELETE`,
+            type: "DELETE",
         }).then(() => location.reload());
     });
+
+
 });
